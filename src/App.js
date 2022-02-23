af614fa68d111dfa134ff7c9b7ab4ffb7417f784
AllySadz <alexandria.sadzius@gmail.com>
2022-02-23 18:28:22 +1100

    Added the githug reppo url

--- src/App.js
+++ src/App.js
@@ -2,5 +2,19 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App </div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://sharp-roentgen-fef16d.netlify.app/" target="_blank">
+          Ally Sadzius {""}
+        </a>{" "}
+        and is {""}
+        <a href="https://github.com/AllySadz/react-weather-app" target="_blank">
+          open-sourced
+        </a>
+      </footer>
+    </div>
+  );
 }
